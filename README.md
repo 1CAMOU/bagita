3494125807ffb954ecb730d0af0273196b067c99
1CAMOU <mgmt.camou@gmail.com>
2021-08-29 00:52:59 +0200

    Updated project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bagita 0.1
+
+Bagita is an open-source, PHP-based and easy-to-use blog CMS that is completely customizable. It helps the user to manage and extend his own blog system completely without any difficulties.
+
+## Roadmap
+
+This project is still a work in progress, so here is a brief roadmap of what features are currently in the works.
+
+-   [x] Blog Posts Single View
+-   [x] Caching Posts
+-   [x] Blog Post Overview
+-   [ ] Filtering Blogs
+-   [ ] Search Functionality
+-   [ ] Pagination
+-   [ ] Comments
+-   [ ] Newsletter Integration
+-   [ ] Editor authentication
+-   [ ] A modern, customizable UI
+-   [ ] Easy blog setup
+-   [ ] CMS Dashboard
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // Find all posts in posts folder and pass an array of Post objects to the "posts" view
+    return view('posts', ['posts' => Post::all()]);
+});
+
+Route::get('/post/{post}', function ($slug) {
+    // Find a post by its slug and pass it to a view called "post"
+    return view('post', ['post' => Post::find($slug)]);
+});
