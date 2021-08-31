33b5cbd8eec4d669750de2b3063553493d10f341
1CAMOU <mgmt.camou@gmail.com>
2021-08-31 19:41:59 +0200

    Responsive navigation & footer

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,13 @@
 <x-layout>
-    <x-post-featured-card :post="$posts->first()" />
-        
-    <div class="lg:grid lg:grid-cols-3 lg:gap-20 lg:gap-y-10 max-w-screen-xl mx-auto">
-        @foreach ($posts->skip(1) as $post)
-            <x-post-card :post="$post" />
-        @endforeach  
-    </div>
+    @if ($posts->count())
+        <x-post-featured-card :post="$posts->first()" />
+            
+        @if ($posts->count() > 1)
+            <div class="lg:grid lg:grid-cols-3 lg:gap-20 lg:gap-y-10 max-w-screen-xl mx-auto">
+                @foreach ($posts->skip(1) as $post)
+                    <x-post-card :post="$post" />
+                @endforeach  
+            </div>
+        @endif
+    @endif
 </x-layout>
