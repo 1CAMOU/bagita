0af3de381c5d311168a0bee6f8d60fc77a34bd06
1CAMOU <mgmt.camou@gmail.com>
2021-09-04 10:49:53 +0200

    Added Pagination 🎊

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,8 +4,12 @@
     @if ($posts->count())
         <x-post-featured-card :post="$posts->first()" />
             
-        @if ($posts->count() > 1)
+        @if ($posts->count())
             <x-post-grid :posts="$posts" />
+
+            {{ $posts->links() }}
+        @else
+            <p class="text-center">No Posts found! Please try again later.</p>
         @endif
     @endif
 </x-layout>
