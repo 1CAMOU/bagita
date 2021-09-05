52fe27ee37b5f032bfa45a5a0e37b93f93750efd
1CAMOU <mgmt.camou@gmail.com>
2021-09-06 01:36:57 +0200

    Registration Form

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/post/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/register', [RegisterController::class, 'create']);
