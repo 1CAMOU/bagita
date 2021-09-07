8f460a47cf0fcda68574918d67733bc0109de449
1CAMOU <mgmt.camou@gmail.com>
2021-09-07 18:58:09 +0200

    Added comment styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -31,7 +31,7 @@
                         <button type="submit" class="text-secondary font-medium">Logout</button>
                     </form>
                 @else
-                    <a href="/register" class="text-secondary font-medium mr-2 md:mr-6">Sign Up</a>
+                    <a href="/register" class="text-secondary font-medium mb-2 md:mb-0 mr-2 md:mr-6">Sign Up</a>
                 @endauth
                 
                 <button class="bg-gray-50 px-6 pt-2 pb-1 rounded-md text-secondary font-semibold" href="">SUBSCRIBE</button>
@@ -46,20 +46,20 @@
         {{ $slot }}
     </main>
 
-    <div class="m-6 bg-gray-100 py-14 rounded-md flex justify-center items-center w-auto mb-12 md:mb-auto">
+    <div class="m-6 bg-gray-100 py-14 rounded-md flex justify-center items-center w-auto mb-12 md:mb-6">
         <div class="flex flex-col justify-center items-center w-full">
-            <svg class="mb-4    " width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg class="mb-4" width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M57 3.56072C56.9997 2.95368 56.8443 2.35679 56.5485 1.82669C56.2527 1.29659 55.8264 0.850862 55.31 0.531799C54.7936 0.212736 54.2042 0.0309214 53.5978 0.0036085C52.9914 -0.0237044 52.3881 0.104389 51.8451 0.375736L24.0932 14.2486H10.6875C7.853 14.2486 5.13459 15.3746 3.1303 17.379C1.126 19.3833 0 22.1018 0 24.9364C0 27.771 1.126 30.4895 3.1303 32.4939C5.13459 34.4982 7.853 35.6243 10.6875 35.6243H11.685L17.9942 54.5632C18.2306 55.2729 18.6843 55.8902 19.2911 56.3275C19.8979 56.7649 20.627 57.0002 21.375 57H24.9375C25.8823 57 26.7885 56.6247 27.4566 55.9565C28.1247 55.2884 28.5 54.3822 28.5 53.4374V37.826L51.8451 49.4971C52.3881 49.7685 52.9914 49.8966 53.5978 49.8693C54.2042 49.8419 54.7936 49.6601 55.31 49.3411C55.8264 49.022 56.2527 48.5763 56.5485 48.0462C56.8443 47.5161 56.9997 46.9192 57 46.3121V3.56072Z" fill="#31E981"/>
             </svg>
             <h3 class="text-xl md:text-2xl text-secondary font-medium text-center break-words">Stay in touch with the latest posts</h3>
             <h4 class="text-md md:text-lg text-secondary text-center">Promise to keep the inbox clean. No spam.</h4>
 
-            <form class="w-11/12 md:w-96 h-9 relative mt-6 flex flex-col md:flex-row bg-gray-200">
+            <form class="w-11/12 md:w-96 h-auto md:h-9 relative mt-6 flex flex-col md:flex-row bg-transparent md:bg-gray-200">
                 <svg class="absolute left-3 top-3" width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0.00299072 1.884L7.99999 5.882L15.997 1.884C15.9674 1.37444 15.7441 0.895488 15.3728 0.545227C15.0015 0.194965 14.5104 -9.35847e-05 14 3.36834e-08H1.99999C1.48958 -9.35847e-05 0.998438 0.194965 0.627168 0.545227C0.255899 0.895488 0.0325948 1.37444 0.00299072 1.884Z" fill="#383E66" fill-opacity="0.6"/>
                     <path d="M16 4.11804L8 8.11804L0 4.11804V10C0 10.5305 0.210714 11.0392 0.585786 11.4143C0.960859 11.7893 1.46957 12 2 12H14C14.5304 12 15.039   1 11.7893 15.4142 11.4143C15.7893 11.0392 16 10.5305 16 10V4.11804Z" fill="#383E66" fill-opacity="0.6"/>
                 </svg>
-                <input class="w-full h-full pt-2 pb-1 pl-10 pr-4 bg-transparent focus:outline-none" type="email" placeholder="Your email address">
+                <input class="w-full h-full pt-2 pb-1 pl-10 pr-4 bg-gray-200 md:bg-transparent focus:outline-none" type="email" placeholder="Your email address">
                 <button class="bg-primary rounded-md px-4 pt-4 pb-3 mt-4 md:mt-0 md:py-0 md:pt-1 md:pb-0 font-semibold text-secondary align-middle">SUBSCRIBE</button>
             </form>
         </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,27 +1,50 @@
 <x-layout>
-    <article class="flex flex-col md:flex-row justify-center my-12 relative">
-        <aside class="flex w-full mb-16 md:mb-0 md:w-auto flex-col items-center mr-24">
-            <img class="" src="/images/content-post.png" alt="Post Thumbnail">
-            <p class="text-gray-400 text-sm mt-4"><time>{{ $post->created_at->format('M d, Y') }}</time> • {{ $post->readDuration() }} min read</p>
-            <a href="/?author={{ $post->author->username }}">
-                <div class="flex items-center mt-6">
-                    <img class="mr-6" src="/images/author.png" alt="Author" width=50 height=50>
-                    <div>
-                        <h5 class="text-secondary font-medium">{{ $post->author->name }}</h5>
-                        <p class="text-gray-400 text-sm">{{ $post->author->username }}</p>
+    <article class="flex flex-col my-12 md:mx-12 lg:mx-24 relative">
+        <div class="flex flex-col md:flex-row justify-between">
+            <aside class="flex w-full mb-16 md:mb-0 md:w-auto flex-col items-center mr-24">
+                <img class="" src="/images/content-post.png" alt="Post Thumbnail">
+                <p class="text-gray-400 text-sm mt-4"><time>{{ $post->created_at->format('M d, Y') }}</time> • {{ $post->readDuration() }} min read</p>
+                <a href="/?author={{ $post->author->username }}">
+                    <div class="flex items-center mt-6">
+                        <img class="mr-6" src="/images/author.png" alt="Author" width=50 height=50>
+                        <div>
+                            <h5 class="text-secondary font-medium">{{ $post->author->name }}</h5>
+                            <p class="text-gray-400 text-sm">{{ $post->author->username }}</p>
+                        </div>
                     </div>
+                </a>
+                
+            </aside>
+    
+            <section class="w-full md:w-1/2">
+                <h4 class="text-blue-700 font-medium">{{ $post->category->name }}</h4>
+                <h2 class="text-secondary font-semibold text-lg mb-6">{{ $post->title }}</h2>
+    
+                <div class="text-secondary space-y-4">
+                    {!! $post->body !!}
+                </div>            
+            </section>
+        </div>
+
+        <section class="mt-24">
+            <article class="flex flex-wrap">
+                <div class="w-12 md:w-20 mr-6 h-32 bg-red-200">
+                    <img src="https://i.pravatar.cc/200" class="rounded-md w-full" alt="">
                 </div>
-            </a>
-            
-        </aside>
 
-        <section class="w-full md:w-1/2">
-            <h4 class="text-blue-700 font-medium">{{ $post->category->name }}</h4>
-            <h2 class="text-secondary font-semibold text-lg mb-6">{{ $post->title }}</h2>
+                <header class="mt-1 md:mt-0 h-auto">
+                    <h3 class="font-bold text-secondary">John Doe</h3>
+                    <p class="text-xs text-secondary">Posted 
+                        <time>
+                            8 months ago
+                        </time>
+                    </p>
+                </header>
 
-            <div class="text-secondary space-y-4">
-                {!! $post->body !!}
-            </div>            
+                <p class="text-secondary mt-6">
+                    Neque dolores nesciunt qui facilis delectus natus. Asperiores doloremque fuga facilis in quos. Ipsum id qui dignissimos nihil cupiditate. Est saepe blanditiis magnam voluptatem beatae odio velit.
+                </p>
+            </article>
         </section>
 
         <a class="text-gray-400 absolute left-0 -bottom-20" href="/">⬅ Back to Posts</a>
