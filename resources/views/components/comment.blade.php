cca9c6a37d048c84ba709730fe443683f75fc85e
1CAMOU <mgmt.camou@gmail.com>
2021-09-09 11:24:26 +0200

    Comment Model & Database constraints

---
+++ resources/views/components/comment.blade.php
@@ -0,0 +1,22 @@
+<article class="h-48 max-w-5xl bg-gray-150 rounded-md mx-auto w-full p-6">
+    <div class="flex flex-wrap h-full relative">
+        <div class="w-12 md:w-20 mr-6 md:h-full">
+            <a href="">
+                <img src="https://i.pravatar.cc/200" class="rounded-md w-full" alt="">
+            </a>
+        </div>
+
+        <header class="mt-1 md:mt-0 md:w-3/4 h-12">
+            <a href="" class="font-bold text-secondary">John Doe</a>
+            <p class="text-xs text-secondary">Posted 
+                <time>
+                    8 months ago
+                </time>
+            </p>
+        </header>
+
+        <p class="text-secondary md:w-3/4 mt-2 md:mt-0 md:absolute md:top-14 md:left-27">
+            Neque dolores nesciunt qui facilis delectus natus. Asperiores doloremque fuga facilis in quos. Ipsum id qui dignissimos nihil cupiditate. Est saepe blanditiis magnam voluptatem beatae odio velit.
+        </p>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,29 +26,13 @@
             </section>
         </div>
 
-        <section class="mt-24 h-48 max-w-5xl bg-gray-150 rounded-md mx-auto w-full p-6">
-            <article class="flex flex-wrap h-full relative">
-                <div class="w-12 md:w-20 mr-6 md:h-full">
-                    <a href="">
-                        <img src="https://i.pravatar.cc/200" class="rounded-md w-full" alt="">
-                    </a>
-                </div>
+        <section class="mt-18 space-y-6">
+            <h2 class="text-center mt-24 font-semibold text-xl text-secondary">Discuss this Topic</h2>
 
-                <header class="mt-1 md:mt-0 md:w-3/4 h-12">
-                    <a href="" class="font-bold text-secondary">John Doe</a>
-                    <p class="text-xs text-secondary">Posted 
-                        <time>
-                            8 months ago
-                        </time>
-                    </p>
-                </header>
-
-                <p class="text-secondary md:w-3/4 mt-2 md:mt-0 md:absolute md:top-14 md:left-27">
-                    Neque dolores nesciunt qui facilis delectus natus. Asperiores doloremque fuga facilis in quos. Ipsum id qui dignissimos nihil cupiditate. Est saepe blanditiis magnam voluptatem beatae odio velit.
-                </p>
-            </article>
+            <x-comment />
+            <x-comment />
         </section>
-
+        
         <a class="text-gray-400 absolute left-0 -bottom-20 hover:text-gray-600 transition-colors" href="/">⬅ Back to Posts</a>
     </article>
 </x-layout>
